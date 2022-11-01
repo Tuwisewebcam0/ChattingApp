49b1a5a31d9d2fad9e37df1730fa359541ebe5c0
Egharevba Nosakhare Nichael <handzome4u@yahoo.com>
2022-11-01 10:30:03 +0000

    Fixed project errors

--- app/src/main/java/com/example/tumingle/SignInActivity.java
+++ app/src/main/java/com/example/tumingle/SignInActivity.java
@@ -9,6 +9,8 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_in_old);
+
+        getSupportActionBar().hide();
     }
 }
